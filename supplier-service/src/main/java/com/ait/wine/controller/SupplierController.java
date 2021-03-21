ece8cde6df9d0d8f6e6d4bff6871639e58b37f8d
Ellen Kennedy <ellenkennedy158@gmail.com>
2021-03-21 14:27:17 +0000

    Additional requests: delete and create suppliers

--- supplier-service/src/main/java/com/ait/wine/controller/SupplierController.java
+++ supplier-service/src/main/java/com/ait/wine/controller/SupplierController.java
@@ -5,13 +5,19 @@ import com.ait.wine.repository.SupplierRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.expression.ExpressionException;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -26,6 +32,26 @@ public class SupplierController {
     public ResponseEntity<Supplier> getSupplierById(@PathVariable long supplierId) throws Exception {
         Supplier supplier = supplierRepository.findById(supplierId).orElseThrow(() -> new Exception("Not found"));
         return ResponseEntity.ok(supplier);
+    }
+
+    @DeleteMapping("/suppliers/{supplierId}")
+    public ResponseEntity deleteSupplierById(@PathVariable long supplierId) throws Exception {
+        Supplier supplier = supplierRepository.findById(supplierId).orElseThrow(() -> new Exception("Not found"));
+        supplierRepository.delete(supplier);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/suppliers")
+    public ResponseEntity addSupplier(@RequestBody Supplier supplier) {
+        supplierRepository.save(supplier);
+        return ResponseEntity.accepted().body(supplier);
+    }
+
+    @GetMapping("/suppliers")
+    public List<Supplier> getSuppliers(@RequestParam("name")Optional<String> name, @RequestParam("country")Optional<String> country) {
+        if (name.isPresent() && country.isPresent()) {
+             return supplierRepository.findByNameAndCountry(name, country);
+        } else if (name.ifPresent(() -> { return supplierRepository.findByName(name);});)
     }
 
 }
