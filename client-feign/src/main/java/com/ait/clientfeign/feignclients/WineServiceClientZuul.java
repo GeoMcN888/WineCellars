13ddf809624208f3bfe6fa5b3dbbdd133f9d098a
George McNally <geomcn11@gmail.com>
2021-03-24 19:54:56 +0000

    edited wineServiceClientZuul mappings

--- client-feign/src/main/java/com/ait/clientfeign/feignclients/WineServiceClientZuul.java
+++ client-feign/src/main/java/com/ait/clientfeign/feignclients/WineServiceClientZuul.java
@@ -1,10 +1,12 @@
 package com.ait.clientfeign.feignclients;
 
 import com.ait.clientfeign.model.Wine;
+import com.ait.wine.model.Supplier;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @FeignClient("zuul-api-gateway")
 public interface WineServiceClientZuul
@@ -14,6 +16,13 @@ public interface WineServiceClientZuul
 
     @DeleteMapping("wine-service/wine/{wineId}")
     void deleteWineById(@PathVariable int wineId);
+
+    @PostMapping("wine-service/wine/{wineId}")
+    Supplier addWine(@RequestBody Wine wine) ;
+
+    @GetMapping("wine-service/wine")
+    public List<Supplier> getWines(@RequestParam("name") Optional<String> name, @RequestParam("country")Optional<String> country);
+
 }
 
 
